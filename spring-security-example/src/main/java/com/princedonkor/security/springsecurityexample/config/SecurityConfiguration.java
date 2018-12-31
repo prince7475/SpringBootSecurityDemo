cb15670697cfe75738c3334550c9eeaeb9da014b
Prince Donkor <Prince.donkor74@gmail.com>
2018-12-30 20:23:31 -0600

    added users with passwords and roles, and now our end points are fully authenticated

--- spring-security-example/src/main/java/com/princedonkor/security/springsecurityexample/config/SecurityConfiguration.java
+++ spring-security-example/src/main/java/com/princedonkor/security/springsecurityexample/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.princedonkor.security.springsecurityexample.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,13 +10,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("prince").password("test").roles("USER").and()
+                .withUser("demo").password("test2").roles("ADMIN");
+    }
+
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .authorizeRequests()
 //                .antMatchers("**//rest/*") -- this is done if you want to match particular urls
                 .anyRequest()
-                .permitAll()
+                .fullyAuthenticated()
                 .and().httpBasic();
             httpSecurity.csrf().disable();
 
