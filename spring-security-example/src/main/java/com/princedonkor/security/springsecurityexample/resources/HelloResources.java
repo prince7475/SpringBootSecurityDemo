8aa5bb153f71aae5eabc83d8d9779d7287869e52
Prince Donkor <Prince.donkor74@gmail.com>
2018-12-30 19:24:48 -0600

    created rest endpoints but current no security due to use having anyRequest in, and permitAll in out httpSecurity

---
+++ spring-security-example/src/main/java/com/princedonkor/security/springsecurityexample/resources/HelloResources.java
@@ -0,0 +1,15 @@
+package com.princedonkor.security.springsecurityexample.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/hello")
+public class HelloResources {
+
+    @GetMapping
+    public String hello() {
+        return "hello world";
+    }
+}
